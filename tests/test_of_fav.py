094506bc659d2e7dc7301dbf03d367a83a99964c
Albert <albertvitt1997@gmail.com>
2023-08-31 20:15:45 +0700

    add_func_for_testing_adding_and_existing_fav

--- tests/test_of_fav.py
+++ tests/test_of_fav.py
@@ -1,22 +1,17 @@
-from resources.locators import Locators
 from pages.item_page import Favourites
 
 
 class TestFav:
      def test_choose_as_fav(self, driver):
-        # Step 1: Navigate to the desired page
+        #Navigate to the desired page
         page = Favourites(driver)
         page.open()
 
-        # Step 2: Click on the favourite button
+        #Click on the favourite button
         page.choose_fav().click()
 
-        # Step 3: Navigate to favourites page
+        #Navigate to favourites page
         page.to_favourites_page().click()
 
-        # Step 4: Check if the item has been added to favourites
+        #Check if the item has been added to favourites
         assert page.is_present_fav(), "The item is not present in favourites"
-
-
-
-
